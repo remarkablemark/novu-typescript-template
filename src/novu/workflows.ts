a568b0bab1acd1bc6bc366832dcb3a0152ca0ec8
Mark <mark@remarkablemark.org>
2026-01-08 13:30:03 -0500

    refactor(novu): create variables for workflow and step ids

--- src/novu/workflows.ts
+++ src/novu/workflows.ts
@@ -1,8 +1,11 @@
 /* istanbul ignore file */
 import { workflow } from '@novu/framework';
 
-export const testWorkflow = workflow('test-workflow', async ({ step }) => {
-  await step.email('test-email', async () => {
+const workflowId = 'test-workflow';
+const stepId = 'test-email';
+
+export const testWorkflow = workflow(workflowId, async ({ step }) => {
+  await step.email(stepId, async () => {
     return {
       subject: 'Test Email',
       body: 'This is a test email from Novu Framework!',
